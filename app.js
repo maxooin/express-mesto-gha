64cbd73df3db42676556dbdc9772aa6ef78c4b50
Mikhail Kotin <maxooin@yandex.ru>
2023-01-25 17:32:48 +0300

    feat: Добавил валидацию Joi при авторизации

--- app.js
+++ app.js
@@ -22,7 +22,17 @@ mongoose.connect('mongodb://localhost:27017/mestodb')
     console.log(`Connection to database was failed with error ${err}`);
   });
 
-app.post('/signin', login);
+app.post('/signin', celebrate({
+  body: Joi.object()
+    .keys({
+      email: Joi.string()
+        .email()
+        .required(),
+      password: Joi.string()
+        .required(),
+    }),
+}), login);
+
 app.post('/signup', celebrate({
   body: Joi.object()
     .keys({
